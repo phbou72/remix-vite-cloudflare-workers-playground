e50f31bbfab47f3d1f2150acedd0a1da75c55264
Philippe Bourdages <1980396+phbou72@users.noreply.github.com>
2024-04-24 15:26:38 -0400

    Add the scripts

---
+++ scripts/duplicateTemplate.ts
@@ -0,0 +1,26 @@
+import { readFile, writeFile } from "node:fs/promises";
+import { createId } from "@paralleldrive/cuid2";
+
+const DUPLICATE_X_TIMES = 100;
+
+async function readTemplateFile(filePath: string): Promise<string> {
+  try {
+    return await readFile(filePath, { encoding: "utf-8" });
+  } catch (error) {
+    console.error("Failed to read file:", error);
+  }
+
+  return "";
+}
+
+async function main() {
+  const templateContent = await readTemplateFile("app/routes/template.tsx");
+
+  for (let i = 0; i < DUPLICATE_X_TIMES; i++) {
+    const id = createId();
+    const newFilePath = `app/routes/${id}.tsx`;
+    writeFile(newFilePath, templateContent);
+  }
+}
+
+main();
